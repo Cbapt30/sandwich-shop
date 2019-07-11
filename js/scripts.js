bacc4beee998a8deebac0f47682459c53c494598
celia baptiste and winter runion <celia+winter@>
2019-07-10 20:17:09 -0700

    Add business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,24 @@ function buildYourOwnBurger (toppings, price) {
   this.toppings = toppings;
   this.price = price;
 }
+
+
+
+//User Interface Logic
+
+
+$(document).ready(function(){
+  $("form#transportation_survey").submit(function(event){
+    event.preventDefault();
+
+    var toppings = $("input:checkbox[name=work-transportation]:checked").val();
+    var newBurger = new buildYourOwnBurger(toppings, price);
+
+    // $(".hidden").show();
+    // $("input:checkbox[name=work-transportation]:checked").each(function(){
+    //   var workTransportationMode = $(this).val();
+    //   $('#work-responses').append(workTransportationMode + "<br>");
+    // });
+    // $('#transportation_survey').hide();
+  });
+});
